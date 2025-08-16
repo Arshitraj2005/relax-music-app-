1a656d0136cd01cef4de93577eb4f93cb5afa93f
Arshitraj2005 <nigachu789@gmail.com>
2025-08-16 15:23:15 +0530

    Update music_engine.py

--- generator/music_engine.py
+++ generator/music_engine.py
@@ -4,13 +4,12 @@ import os
 
 def generate_music():
     os.makedirs("static/audio", exist_ok=True)
-    sample_rate = 44100  # Hz
-    duration = 10        # seconds
-    freq = 440.0         # A4 note
+    sample_rate = 44100
+    duration = 10
+    freq = 440.0
 
     t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
     waveform = 0.5 * np.sin(2 * np.pi * freq * t)
-
-    # Convert to 16-bit PCM
     audio = np.int16(waveform * 32767)
+
     write("static/audio/generated_track.wav", sample_rate, audio)
