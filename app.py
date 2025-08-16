256be3bb02e83a719c8a2c6bc49ac137bee534c1
Arshitraj2005 <nigachu789@gmail.com>
2025-08-16 15:23:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
-from utils.weather_fetcher import get_weather_data
+from flask import Flask, render_template, send_from_directory
 from generator.music_engine import generate_music
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    weather = get_weather_data()
-    generate_music(weather)
-    return render_template("index.html", weather=weather)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/generate')
+def generate():
+    generate_music()
+    return render_template('index.html', generated=True)
+
+@app.route('/download')
+def download():
+    path = os.path.join('static', 'audio')
+    return send_from_directory(path, 'generated_track.wav', as_attachment=True)
+
+if __name__ == '__main__':
     app.run(debug=True)
